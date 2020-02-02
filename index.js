492b5d27dae7e51274fc2013bbc428165046aaab
Maruth51 <Maruth@gmail.com>
2020-02-02 09:37:35 +0530

    express-start

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const http =require("http")
-const getquery = require("./getquerystring")
-//callback functions are invoked when request is made to the server
 
-http.createServer((req,res)=>{
-    const queryobject = getquery(req.url).query
-    console.log(queryobject)
-    res.writeHead(200,{"content-type" : "application/json"}) // mime type(
-    res.write(JSON.stringify(queryobject))
-    res.end();
+const express = require("express")
+
+const app = express()
+app.get("/",(req,res)=>{
+    res.send("Home ")
+})
+
+app.get("/profile",(req,res)=>{
+    res.send("Profile page")
 }
+)
+const server = app.listen("8080", ()=>{
+    console.log(`Server runninng on port: ${server.address().port}`)
+})
 
-).listen(8080);
+
+
+
+// const http =require("http")
+// const getquery = require("./getquerystring")
+// //callback functions are invoked when request is made to the server
+
+// http.createServer((req,res)=>{
+//     const queryobject = getquery(req.url).query
+//     console.log(queryobject)
+//     res.writeHead(200,{"content-type" : "application/json"}) // mime type(
+//     res.write(JSON.stringify(queryobject))
+//     res.end();
+// }
+
+// ).listen(8080);
 
